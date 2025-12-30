d4a3e5c1f25c1d880bccb5705b8910aacb58b42a
Swrzy505 <singhshikhar1004@gmail.com>
2025-12-30 21:49:02 +0530

    done

--- BASICS/typedef.cpp
+++ BASICS/typedef.cpp
@@ -3,9 +3,13 @@
 
 // typedef std::vector<std::pair<std::string,int>> pairlist_t;
 
-typedef std::string text_t;
-typedef int num_t;
+// typedef std::string text_t;
+// typedef int num_t;
 
+// typdef = using same thing
+
+using text_t = std::string;
+using num_t = int;
 int main(){
     // typedef = reserved keyword used to create an additional name (alias) for another data type.New identifer for an existting type helps with readibiltiy and reduces typos 
     text_t firstname = "Shikhar";
